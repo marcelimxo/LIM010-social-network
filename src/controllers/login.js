b9f2e4065d9be718e40934ca25bb789715d1f4e6
jorgelisari1 <jorgelis_ari@hotmail.com>
2019-08-08 12:43:51 -0500

    registrar usuario en firesbase

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,6 +1,27 @@
+
 import { loginPage } from '../views/login.js';
 
 export default () => {
   //
   document.getElementById('root').innerHTML = loginPage;
+  
+  const buttonLogin = document.getElementById('button-login');
+  buttonLogin.addEventListener('click', () => {
+    const emailvalue = document.getElementById('email').value;
+    const passwordvalue = document.getElementById('password').value;
+
+    firebase.auth().createUserWithEmailAndPassword(emailvalue, passwordvalue)
+      .then(() => {
+        console.log('usuario Registrado')
+      })
+      .catch((error) => {
+  
+        console.log('error de codigo:' + error.code);
+        console.log('mensaje de error:' + error.message);
+      })
+  })
+
+
+
 };
+
--- src/views/login.js
+++ src/views/login.js
@@ -2,12 +2,13 @@ export const loginPage = `
 <div id="inicio">
       <h1>¡Bienvenida, a Poematik!</h1>
       <form class="login" id="content-inicio">
-        <input class="radius" type="text" id="email" placeholder="  Email">
+        <input class="radius" type="email" id="email" placeholder="  Email">
         <input class="radius" type="password" id="password" placeholder="  Password">
-        <p><button id="boton-ingresar" class="radius" type="button" onClick="window.location.href = '/#/home'"> Log in </button></p>
+        <p><button id="button-login" class="radius" type="button"> Log in </button></p>
         <p>O bien ingresa con...</p>
         <div><img class="logo" src="img/facebook.png" alt="logo de facebook">
           <img class="logo" src="img/google.png" alt="logo de google"></div>
+          <button id="google" class="radius" type="button"> google </button>
         <label class="registrate">¿No tienes una cuenta? <strong>Registrate</strong></label>
       </form>
     </div>
