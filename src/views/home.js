1bccf0413702a65136b88645977957c2f089d8f9
marcelimxo <47229976+marcelimxo@users.noreply.github.com>
2019-08-15 17:52:56 -0500

    Probando

--- src/views/home.js
+++ src/views/home.js
@@ -4,5 +4,3 @@ export default (username) => {
     <p>¡Bienvenidx, ${username}!</p>`;
   document.getElementById('root').innerHTML = welcomeMsg;
 };
-/* <a id="sign-out">
-<img class="social-media-img" src="/src/img/sign-out.svg" alt="sign-out" > </a> */
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,41 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
+import {
+  login, registerWithEmail, registerUserGoogle,
+  registerUserFacebook, addUserToFirestore, getUserInfo,
+} from '../src/models/users.js';
 
-describe('myFunction', () => {
+describe('login', () => {
   it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+    expect(typeof login).toBe('function');
+  });
+});
+
+describe('registerWithEmail', () => {
+  it('debería ser una función', () => {
+    expect(typeof registerWithEmail).toBe('function');
+  });
+});
+
+describe('registerUserGoogle', () => {
+  it('debería ser una función', () => {
+    expect(typeof registerUserGoogle).toBe('function');
+  });
+});
+
+describe('registerUserFacebook', () => {
+  it('debería ser una función', () => {
+    expect(typeof registerUserFacebook).toBe('function');
+  });
+});
+
+describe('addUserToFirestore', () => {
+  it('debería ser una función', () => {
+    expect(typeof addUserToFirestore).toBe('function');
+  });
+});
+
+describe('getUserInfo', () => {
+  it('debería ser una función', () => {
+    expect(typeof getUserInfo).toBe('function');
   });
 });
