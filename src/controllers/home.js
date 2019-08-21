bb8df107874794c8a41fba1cda5625fdbdd63b47
marcelimxo <47229976+marcelimxo@users.noreply.github.com>
2019-08-21 10:38:41 -0500

    Actualizando vista y controlador home

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,8 +1,25 @@
 import homePage from '../views/home.js';
 import { getUserInfo } from '../models/users.js';
+import { addPost } from '../models/posts.js';
 
 export default async () => {
   const { uid } = await firebase.auth().currentUser;
   const { name } = await getUserInfo(uid);
+
   homePage(name);
+
+  const btnPost = document.getElementById('button-post');
+
+  const addPostOnSubmit = (evt) => {
+    evt.preventDefault();
+    firebase.auth().onAuthStateChanged(() => {
+      if (uid) {
+        const inputText = document.getElementById('post-text');
+        addPost(inputText.value, uid);
+        inputText.value = '';
+      }
+    });
+  };
+
+  btnPost.addEventListener('click', addPostOnSubmit);
 };
