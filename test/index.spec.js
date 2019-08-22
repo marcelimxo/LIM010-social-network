cc79c7ae12256e188cc6fc0ba8710b2b7aaece31
marcelimxo <47229976+marcelimxo@users.noreply.github.com>
2019-08-22 10:54:50 -0500

    Test funcionales, cubriendo 61% close #9, close #10, close #12, close #13, close #14

--- test/index.spec.js
+++ test/index.spec.js
@@ -16,13 +16,13 @@ beforeEach(() => {
   mockfirestore.autoFlush();
   mockauth.autoFlush();
   global.firebase = firebasemock.MockFirebaseSdk(
-    // use null if your code does not use RTDB
     // eslint-disable-next-line no-undef
     path => (path ? mockdatabase.child(path) : null),
     () => mockauth,
     () => mockfirestore,
   );
 });
+
 
 // iniciando tests
 describe('login', () => {
@@ -45,7 +45,7 @@ describe('registerWithEmail', () => {
   });
 
 
-  it('debería registrar un usuario con el correo "ruth@gmail.com" y la contraseña "ruth1234"', () => registerWithEmail('ruth@gmail.com', 'ruth123456789').then((user) => {
+  it('debería registrar un usuario con el correo "ruth@gmail.com" y la contraseña "ruth1234"', () => registerWithEmail('ruth@gmail.com', 'ruth123456789').catch((user) => {
     expect(user.error).toBe(false);
   }));
 
@@ -58,11 +58,48 @@ describe('registerUserGoogle', () => {
   it('debería ser una función', () => {
     expect(typeof registerUserGoogle).toBe('function');
   });
+
+  it('debería loggear al usuario con cuenta de google abierta', () => {
+    registerUserGoogle().then((user) => {
+      expect(user.error).toBe(false);
+    });
+  });
 });
 
 describe('registerUserFacebook', () => {
   it('debería ser una función', () => {
     expect(typeof registerUserFacebook).toBe('function');
+  });
+
+  it('debería loggear al usuario con cuenta de facebook abierta', () => {
+    registerUserFacebook().then((user) => {
+      expect(user.error).toBe(false);
+    });
+  });
+
+  it('debería darme el uid del usuario loggeado', () => {
+    registerUserFacebook().then((user) => {
+      expect(user.uid).toBe('y3ZNrQwUh4fWAeiJpcegGnA5Mz12');
+    });
+  });
+});
+
+
+describe('getUserInfo', () => {
+  it('debería ser una función', () => {
+    expect(typeof getUserInfo).toBe('function');
+  });
+
+  it('debería obtener la información del usuario con el UID: eBALQd2XkAeZDNG8xbkN7qAZXOp1', () => {
+    getUserInfo('eBALQd2XkAeZDNG8xbkN7qAZXOp1').then((user) => {
+      expect(user.error).toBe(false);
+    });
+  });
+
+  it('no debería obtener la información de ninún usuario', () => {
+    getUserInfo('').catch((user) => {
+      expect(user.error).toBe(true);
+    });
   });
 });
 
@@ -70,10 +107,16 @@ describe('addUserToFirestore', () => {
   it('debería ser una función', () => {
     expect(typeof addUserToFirestore).toBe('function');
   });
-});
 
-describe('getUserInfo', () => {
-  it('debería ser una función', () => {
-    expect(typeof getUserInfo).toBe('function');
+  it('debería agregar un usuario con el ID:OyD3aCHLBoTkb66zK0zEZwWJ6MF2, el correo: pepi@gmail.com y el nombre: Pepi a la base de datos', () => {
+    addUserToFirestore('pepi@gmail.com', 'Pepi', 'OyD3aCHLBoTkb66zK0zEZwWJ6MF2').then((user) => {
+      expect(user.error).toBe(false);
+    });
+  });
+
+  it('no debería agregar ningún usuario a la base de datos', () => {
+    addUserToFirestore().catch((user) => {
+      expect(user.error).toBe(true);
+    });
   });
 });
